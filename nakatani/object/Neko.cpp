dd913039b90e77f5c8d5e1201710649c5f04e5d1
nakatani-kazuki <futurenaraz1984@gmail.com>
2014-05-06 18:21:04 +0900

    猫の名前と鳴き声を決めれるように改修

--- nakatani/object/Neko.cpp
+++ nakatani/object/Neko.cpp
@@ -7,20 +7,31 @@ class Neko
 {
 private:
     string name;
+    string cry;
 public:
     Neko(string s){
         name = s;
     }
+    void set_cry(string s){
+        cry = s;
+    }
     void naku(){
-        cout<<"にゃあ。俺様は"<<name<<"だ。"<<endl;
+        cout<<cry<<"。俺様は"<<name<<"だ。"<<endl;
     }
 };
 
 int main()
 {
-    Neko dora("ボス");  //「ボス」というの名前を持ったdoraが生成される
-    cout<<"あなたの名づけた猫がメモリ上に生成されました。"<<endl;
-    cout<<"猫が鳴きます。"<<endl;
-
+    string  s;
+    string  s2;
+    cout << "どら猫を生成します。名前を入力してください。" << endl;
+    cin >> s;
+    Neko dora(s);
+    cout << "あなたの名づけた猫がメモリ上に生成されました。" << endl;
+    cout << "鳴き声を決めて下さい" << endl;
+    cin >> s2;
+    dora.set_cry(s2);
+    cout << "猫が鳴きます。" << endl;
     dora.naku();
 }
+
