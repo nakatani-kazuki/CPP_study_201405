047bbab6d1ef37ff29960725bdd2c4d1b16a8f6e
nakatani-kazuki <futurenaraz1984@gmail.com>
2014-05-06 17:09:42 +0900

    対話式のプログラムを書いてみた

---
+++ nakatani/hello2.cpp
@@ -0,0 +1,13 @@
+//hello2.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "こんにちは。私はコンピュータです。" <<endl;
+    cout << "あなたの名前を入力してください。" <<endl;
+    cin >> name;
+    cout<<name<<"さん。よろしく。"<<endl;
+}
